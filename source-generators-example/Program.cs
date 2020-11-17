40095d615ed108ff865c17c3f8ab21c8bfc99cd5
Dmitry Afonchenko <undermove@yandex.ru>
2020-11-18 00:20:51 +0300

    Generated Example Works

---
+++ source-generators-example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace source_generators_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldGenerated.HelloWorld.SayHello(); // calls Console.WriteLine("Hello World!") and then prints out syntax trees
+        }
+    }
+}
